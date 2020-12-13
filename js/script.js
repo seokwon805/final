1776b64fea39d67ab062b993dd6b389e82163554
seokwon805 <71585682+seokwon805@users.noreply.github.com>
2020-12-14 08:42:04 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,258 @@
+  
+$(document).ready(function(){
+
+ 
+	console.log($(".st0"))
+    var pathArray = $(".st0")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+   	console.log($(".st1"))
+    var pathArray = $(".st1")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+    console.log($(".st2"))
+    var pathArray = $(".st2")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+    console.log($(".st3"))
+    var pathArray = $(".st3")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+    console.log($(".st4"))
+    var pathArray = $(".st4")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+       console.log($(".st5"))
+    var pathArray = $(".st5")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+          console.log($(".st6"))
+    var pathArray = $(".st6")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+             console.log($(".st7"))
+    var pathArray = $(".st7")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                console.log($(".st8"))
+    var pathArray = $(".st8")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+             console.log($(".st9"))
+    var pathArray = $(".st9")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                console.log($(".st10"))
+    var pathArray = $(".st10")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                   console.log($(".st11"))
+    var pathArray = $(".st11")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                console.log($(".st12"))
+    var pathArray = $(".st12")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+                console.log($(".st13"))
+    var pathArray = $(".st13")
+
+
+
+   $.each(pathArray, function(i, d) {
+       console.log(i, d);
+
+
+       var pathLength = d.getTotalLength()
+       console.log(pathLength)
+
+       $(d).css("stroke-dasharray", pathLength + " " + pathLength)
+       $(d).css("stroke-dashoffset",pathLength)
+
+   });
+
+
+
+
+
+
+
+
+
+
+
+})
+ 
+
+
